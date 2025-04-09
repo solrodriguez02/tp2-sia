11d3c669cfcbfa89cb8a32042a3ce0b3caa6bbc7
solrodriguezg <solrodriguez@itba.edu.ar>
2025-04-09 16:17:30 -0300

    Generation of initial population and turning individuals into canvas

---
+++ create_individuals.py
@@ -0,0 +1,42 @@
+import random
+from individual import Triangle, Canvas
+
+def create_individuals(num_individuals, num_triangles, width=500, height=500):
+    
+    individuals = []
+    
+    for _ in range(num_individuals):
+        individual = Canvas()
+
+        for _ in range(num_triangles):
+            # Genero el triángulo con 3 vértices random y un color random
+            vertexes = generate_vertexes(width, height)
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.uniform(0, 1))
+
+            triangle = Triangle(vertexes, color)
+            individual.add_triangle(triangle)
+                    
+        individuals.append(individual)
+
+    return individuals
+
+
+def generate_vertexes(width, height):
+   # Genero el primer vértice random
+    cx = random.randint(0, width - 1)
+    cy = random.randint(0, height - 1)
+
+    vertexes = [(cx, cy)]
+
+    # Para los dos vertices restantes, elijo un desplazamiento aleatorio (a partir del primer vértice) 
+    for _ in range(2):
+        x = cx + random.randint(-width, width)
+        y = cy + random.randint(-height, height)
+
+        # Si el vértice se sale del canvas, lo ajusto a los límites
+        x = max(0, min(width - 1, x))
+        y = max(0, min(height - 1, y))
+
+        vertexes.append((x, y))
+
+    return vertexes
---
+++ generate_canvas.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw
+from create_individuals import create_individuals
+
+def canvas_to_image(canvas, width=500, height=500):
+    image = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+
+    for triangle in canvas.triangles:
+        triangle_temp = Image.new('RGBA', (width, height), (0, 0, 0, 0))    # creo el template para el triángulo
+        draw = ImageDraw.Draw(triangle_temp, 'RGBA')    # permite dibujar sobre el template
+
+        vertexes = triangle.vertexes
+        r, g, b, a = triangle.color
+        alpha = int(a * 255)
+
+        draw.polygon(vertexes, fill=(r, g, b, alpha))   # dibujo el triángulo en el template
+        image = Image.alpha_composite(image, triangle_temp)  # superpongo el triángulo al canvas
+
+    return image
+
+
+if __name__ == "__main__":
+    individuals = create_individuals(1, 50) 
+    image = canvas_to_image(individuals[0])
+    image.show()
+    image.save("output.png")
