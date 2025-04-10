2389594508e2020eef8648615a069892344cbf1d
UrielArias <urielarias.a12@gmail.com>
2025-04-10 06:58:03 -0300

    refactor: adding functions to update properties for each triangle based on mutated genes

--- models/individual.py
+++ models/individual.py
@@ -24,6 +24,26 @@ class Triangle:
             #change last parameter if image dimensions' restrictions are changed
             self.genes.append(PositionGene(vertex_names[vertex_index], self.vertexes[vertex_index], (499,499)))
 
+    def update_from_gene(self, gene):
+        if isinstance(gene, ColorGene):
+            match gene.name:
+                case "Red":
+                    self.color = (gene.value, self.color[1], self.color[2], self.color[3])
+                case "Green":
+                    self.color = (self.color[0], gene.value, self.color[2], self.color[3])
+                case "Blue":
+                    self.color = (self.color[0], self.color[1], gene.value, self.color[3])
+        elif isinstance(gene, OpacityGene):
+            self.color = (self.color[0], self.color[1], self.color[2], gene.value)
+        elif isinstance(gene, PositionGene):
+            match gene.name:
+               case "Vertex1": 
+                    self.vertexes[0] = gene.value
+               case "Vertex2": 
+                    self.vertexes[1] = gene.value
+               case "Vertex3":  
+                    self.vertexes[2] = gene.value
+
 class Canvas:
     def __init__(self):
         self.triangles = []
@@ -31,4 +51,9 @@ class Canvas:
 
     def add_triangle(self, triangle):
         self.triangles.append(triangle)
-        self.chromosome.append(triangle.genes)
+        self.chromosome.extend(triangle.genes)
+
+    def update_triangle_from_gene(self, gene, gene_position):
+        # considering each triangle has 7 genes
+        triangle_index = gene_position / 7
+        self.triangles[triangle_index].update_from_gene(gene)
