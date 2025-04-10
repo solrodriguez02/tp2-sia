6474bc31fcd40f0d60c77684a58866f1c9770de0
micaelaperillo <micaelaperillo26@gmail.com>
2025-04-10 10:30:14 -0300

    feat: boltzmann selection algorithm

---
+++ genetic_algorithm/selection_algorithms/boltzmann_selection.py
@@ -0,0 +1,29 @@
+from math import exp
+from random import uniform
+from itertools import accumulate
+
+class BoltzmannSelection:
+
+    def __init__(self, size):
+        self.size = size
+
+    def select(self, population, fitness_function, temperature):
+        
+        # Pseudo fitness aptitude
+        exp_values = [exp(fitness_function(individual) / temperature) for individual in population]
+        avg_exp = sum(exp_values) / len(exp_values)
+        pseudo_fitnesses = [x / avg_exp for x in exp_values]
+
+        q = list(accumulate(pseudo_fitnesses))
+
+        selected_individuals = []
+
+        for _ in range(self.size):
+            r = uniform(0, 1)
+            for i, qi in enumerate(q):
+                q_prev = q[i - 1] if i > 0 else 0
+                if q_prev <= r < qi:
+                    selected_individuals.append(population[i])
+                    break        
+        
+        return selected_individuals
