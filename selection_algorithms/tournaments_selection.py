0e185844a27d059a0d9b3db3294b931bf5b538ea
UrielArias <urielarias.a12@gmail.com>
2025-04-10 06:56:03 -0300

    refactor: changing run function  for generalization in tournaments selection

--- selection_algorithms/tournaments_selection.py
+++ selection_algorithms/tournaments_selection.py
@@ -3,20 +3,19 @@ import random
 class TournamentSelection:
     #providing the fitness fuction gives a higher value if the individual is similar to the target image
     # rounds = K
-    def __init__(self, fitness_function, rounds, M, threshold=1.0):
-        self.fitness_function = fitness_function
+    def __init__(self, rounds, M, threshold=1.0):
         self.rounds = rounds
         self.M = M
         self.threshold = threshold
 
-    def runDeterministicTournament(self, population):
+    def runDeterministicTournament(self, population, fitness_function):
         best_individuals = set()
         for round in range(self.rounds):
             best_individual = population[0]
             best_score = -1
             for i in range(self.M):
                 random_individual = population[random.randint(0,len(population)-1)]
-                current_score = self.fitness_function(random_individual)
+                current_score = fitness_function(random_individual)
                 if current_score > best_score:
                     best_score = current_score
                     best_individual = random_individual
@@ -24,7 +23,7 @@ class TournamentSelection:
         
         return best_individuals
 
-    def runProbabilisticTournament(self, population):
+    def runProbabilisticTournament(self, population, fitness_function):
         best_individuals = set() 
         for round in range(self.rounds):
             best_individual = population[0]
@@ -33,7 +32,7 @@ class TournamentSelection:
             first_individual = population[random.randint(0, len(population)-1)]
             second_individual = population[random.randint(0, len(population)-1)]
 
-            if (self.fitness_function(first_individual) > self.fitness_function(second_individual)):
+            if (self.fitness_function(first_individual) > fitness_function(second_individual)):
                 best_individual = first_individual
                 worse_individual = second_individual
             else:
