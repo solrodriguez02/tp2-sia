73bc3008940aaf2c339f5723aee2ad025f692644
micaelaperillo <micaelaperillo26@gmail.com>
2025-04-13 10:37:02 -0300

    feat: read parameters from config.json

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ from genetic_algorithm.algorithm_definition import GeneticAlgorithm
 from genetic_algorithm.utils.generate_canvas import canvas_to_image
 from genetic_algorithm.fitness import FitnessFunction
 import cv2
+import json
 
 if __name__ == "__main__":
-    # change to use parameters
-    target_image_path = "./images/bandera.jpg"
-    target_image = cv2.imread(target_image_path)
 
-    #read from config file (now all have a default value anyway)
-    initial_population_size = 50
-    triangles_per_solution = 10
-    recombination_probability = 1.0
-    mutation_probability = 1.0
+    with open("config.json", 'r') as config_file:
+        config = json.load(config_file)
+        target_image_path = config.get("target_image_path")
+        initial_population_size = config.get("initial_population_size")
+        triangles_per_solution = config.get("triangles_per_solution")
+        recombination_probability = config.get("recombination_probability")
+        mutation_probability = config.get("mutation_probability")
+        rounds = config.get("rounds")
 
-    fitness_function = FitnessFunction(target_image).fitness_avg_pixel_difference
-    genetic_algorithm = GeneticAlgorithm(fitness_function, target_image, initial_population_size)
-    best_individual, fitness_value, generation = genetic_algorithm.run(triangles_per_solution, recombination_probability, mutation_probability)
-
-    answer_image = canvas_to_image(best_individual)
-    answer_image.save("output_image.png")
-    print(f"The best result was found on generation {generation} with a fitness value of: {fitness_value}")
-
+    for image in target_image_path:
+        for pop_size in initial_population_size:
+            for triangles in triangles_per_solution:
+                for recombination in recombination_probability:
+                    for mutation in mutation_probability:
+                        for r in rounds:
+                            target_image = cv2.imread(image)
+                            fitness_function = FitnessFunction(target_image).fitness_avg_pixel_difference
+                            genetic_algorithm = GeneticAlgorithm(fitness_function, target_image, pop_size, r)
+                            best_individual, fitness_value, generation = genetic_algorithm.run(triangles, recombination, mutation)
+                            answer_image = canvas_to_image(best_individual)
+                            answer_image.save(f"output_image_r{r}.png")
+                            print(f"The best result was found on generation {generation} with a fitness value of: {fitness_value}")
