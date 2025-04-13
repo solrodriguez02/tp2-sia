b9dbc72c134ebecea0288e3b978d6085336405da
solrodriguezg <solrodriguez@itba.edu.ar>
2025-04-13 20:51:22 -0300

    Adding decreasing temperature function to Boltzmann Algorithm

--- genetic_algorithm/selection_algorithms/boltzmann_selection.py
+++ genetic_algorithm/selection_algorithms/boltzmann_selection.py
@@ -1,14 +1,24 @@
 from math import exp
 from genetic_algorithm.utils.create_individuals import random_generator
 from itertools import accumulate
+import math
 
 class BoltzmannSelection:
 
-    def __init__(self, size):
+    def __init__(self, size, total_generations, T0=1.0, with_decreasing_function=False):
         self.size = size
+        self.total_generations = total_generations
+        self.with_decreasing_function = with_decreasing_function
+        self.T0 = T0
+        self.Tc = 0.01
 
-    def select(self, population, fitness_function, temperature):
+    def select(self, population, fitness_function, generation_number):
         
+        if self.with_decreasing_function:
+            temperature = self.temperature_schedule(generation_number)
+        else:
+            temperature = self.T0
+
         # Pseudo fitness aptitude
         exp_values = [exp(fitness_function(individual) / temperature) for individual in population]
         avg_exp = sum(exp_values) / len(exp_values)
@@ -27,3 +37,8 @@ class BoltzmannSelection:
                     break        
         
         return selected_individuals
+    
+    def temperature_schedule(self, generation_number):
+        epsilon = 0.001
+        k = -math.log(epsilon / (self.T0 - self.Tc)) / self.total_generations
+        return self.Tc + (self.T0 - self.Tc) * math.exp(-k * generation_number)
