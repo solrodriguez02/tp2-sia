998482b4e588852f51fd04f0050be10f1b40b642
UrielArias <urielarias.a12@gmail.com>
2025-04-13 00:05:48 -0300

    fix: int not iterable in multiple genes mutation

--- genetic_algorithm/mutation.py
+++ genetic_algorithm/mutation.py
@@ -14,14 +14,14 @@ class Mutation:
                 selected_gene.mutate()
                 individual.update_triangle_from_gene(selected_gene, random_gene_index)
 
-    def mutateMultiplenGenes(self, new_generation):
+    def mutateMultipleGenes(self, new_generation):
         M = self.triangles_per_solution*7
 
         for individual in new_generation:
             genes_mutated = random.randint(1, M)
             change_probability = random.random()
             if change_probability < self.mutation_probability:
-                for i in genes_mutated:
+                for i in range(genes_mutated):
                     random_gene_index = random.randint(0, len(individual.chromosome)-1)
                     selected_gene = individual.chromosome[random_gene_index]
                     selected_gene.mutate()
