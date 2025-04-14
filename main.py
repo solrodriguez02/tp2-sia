dbc5648193182d9860753a96beed32558c5c1821
UrielArias <urielarias.a12@gmail.com>
2025-04-14 00:11:39 -0300

    refactor: adding parents_selection_percentages to config file and main

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
         recombination_probability = config.get("recombination_probability")
         mutation_probability = config.get("mutation_probability")
         rounds = config.get("rounds")
+        parents_selection_percentages = config.get("parents_selection_percentage")
 
     for image in target_image_path:
         img_name = image.split('.')[1]
@@ -27,12 +28,13 @@ if __name__ == "__main__":
             for triangles in triangles_per_solution:
                 for recombination in recombination_probability:
                     for mutation in mutation_probability:
-                        for r in rounds:
-                            target_image = cv2.imread(image)
-                            fitness_function = FitnessFunction(target_image).fitness_avg_pixel_difference
-                            genetic_algorithm = GeneticAlgorithm(fitness_function, target_image, pop_size, r)
-                            best_individual, fitness_value, generation = genetic_algorithm.run(triangles, recombination, mutation)
-                            answer_image = canvas_to_image(best_individual)
-                            answer_image.save(f"outputs/{img_name}_p{pop_size}_t{triangles}_rec{recombination}_mut{mutation}_r{r}.png")
-                            print(f"The best result was found on generation {generation} with a fitness value of: {fitness_value}")
+                        for parents_selection_percentage in parents_selection_percentages:
+                            for r in rounds:
+                                target_image = cv2.imread(image)
+                                fitness_function = FitnessFunction(target_image).fitness_avg_pixel_difference
+                                genetic_algorithm = GeneticAlgorithm(fitness_function, target_image, pop_size, r)
+                                best_individual, fitness_value, generation = genetic_algorithm.run(triangles, recombination, mutation)
+                                answer_image = canvas_to_image(best_individual)
+                                answer_image.save(f"outputs/{img_name}_p{pop_size}_t{triangles}_rec{recombination}_mut{mutation}_r{r}.png")
+                                print(f"The best result was found on generation {generation} with a fitness value of: {fitness_value}")
 
